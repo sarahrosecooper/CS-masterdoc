f86cfe16e75a8b2223b4f93774579b536a9ca2e4
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-06-09 20:33:36 -0400

    1.3

---
+++ 1.3/Binary/balancedBinaryTree.py
@@ -0,0 +1,52 @@
+"""
+You are given a binary tree and you need to write a function that can determine if it is height-balanced.
+A height-balanced tree can be defined as a binary tree in which the left and right subtrees of every node differ in height by a maximum of 1.
+Example 1:
+Given the following tree [5,10,25,None,None,12,3]:
+    5
+   / \
+ 10  25
+    /  \
+   12   3
+return True.
+Example 2:
+Given the following tree [5,6,6,7,7,None,None,8,8]:
+       5
+      / \
+     6   6
+    / \
+   7   7
+  / \
+ 8   8
+return False.
+[execution time limit] 4 seconds (py3)
+[input] tree.integer root
+[output] boolean
+"""
+
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def balancedBinaryTree(root):
+    def get_height(root):
+
+        # Case at Root
+        if root is None:
+            return 0
+
+            # we traverse down the left and right sides
+        left_height = get_height(root.left)
+        right_height = get_height(root.right)
+            
+            # the tree is not balanced, one side is longer than the other
+        if left_height < 0 or right_height < 0 or abs(left_height - right_height) > 1:
+            return -1
+
+            # the tree is balanced
+        return max(left_height, right_height) + 1
+
+    return (get_height(root) >= 0)
